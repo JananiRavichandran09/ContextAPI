c773eeeec717d0f9444b68c9dca042d0ae6b9edc
JananiRavichandran09 <jananiravichandran2001@gmail.com>
2024-03-23 00:12:36 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,62 +7,48 @@ const Navbar = () => {
   const { cart, setCart } = useContext(cartContext);
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-          <a class="navbar-brand" href="#!">
-            <Link
-              to="/"
-              style={{
-                color: "brown",
-                fontFamily: "Mogra, system-ui",
-                fontSize: "2rem",
-                textDecoration: "none",
-              }}
-            >
-              Brand World
-            </Link>
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-              <li class="nav-item">
-                <a class="nav-link" href="#!"></a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <button
-                class=" btn-outline-dark"
-                type="submit"
+      <nav className="navbar bg-body-tertiary">
+        <div className="container">
+          <span className="navbar-brand mb-0 h1">
+            <a className="navbar-brand" href="#!">
+              <Link
+                to="/"
                 style={{
-                  borderRadius: "5px",
-                  backgroundColor: "orange",
-                  border: "none",
-                  padding: "5px 15px",
+                  color: "brown",
+                  fontFamily: "Mogra, system-ui",
+                  fontSize: "2rem",
+                  textDecoration: "none",
                 }}
               >
-                <Link
-                  to="/cartPage"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  Cart
-                  <span class="badge bg-dark text-white ms-1 rounded-pill">
-                    {" "}
-                    {cart.length}{" "}
-                  </span>
-                </Link>
-              </button>
-            </form>
-          </div>
+                Brand World
+              </Link>
+            </a>
+            
+          </span>
+          <span>
+            <button
+              className=" btn-outline-dark"
+              type="submit"
+              style={{
+                borderRadius: "5px",
+                backgroundColor: "orange",
+                border: "none",
+                padding: "5px 15px",
+              }}
+            >
+              <Link
+                to="/cartPage"
+                style={{ color: "black", textDecoration: "none" }}
+              >
+                Cart
+                <span className="badge bg-dark text-white ms-1 rounded-pill">
+                  
+                  {cart.length}
+                </span>
+              </Link>
+            </button>
+            
+          </span>
         </div>
       </nav>
       <br />
@@ -72,3 +58,9 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+
+ 
